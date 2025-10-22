36476421fbfec82e6f8a955185def34c33cfb190
Vladislav Mironov <mironov.v.v@outlook.com>
2025-10-22 18:27:38 +0300

    ReviewController route patterns changed

--- Pawlin.Server/Controllers/ReviewController.cs
+++ Pawlin.Server/Controllers/ReviewController.cs
@@ -15,7 +15,7 @@ namespace Pawlin.Server.Controllers
         : ControllerBase
     {
 
-        [HttpGet("next")]
+        [HttpGet("{deckInstanceId:int}/next")]
         public async Task<ActionResult<FlashcardDto>> GetNextCard(int deckInstanceId)
         {
             var deckInstance = await deckRepository.GetDeckInstance(deckInstanceId);
@@ -24,10 +24,10 @@ namespace Pawlin.Server.Controllers
             return nextFlashcard.Adapt<FlashcardDto>();
         }
 
-        [HttpPost("review")]
-        public async Task<ActionResult<ReviewDataItemDto>> ReviewFlashcard(ReviewDto dto)
+        [HttpPost("{deckInstanceId:int}/review")]
+        public async Task<ActionResult<ReviewDataItemDto>> ReviewFlashcard(int deckInstanceId, [FromBody] ReviewDto dto)
         {
-            var deckInstance = await deckRepository.GetDeckInstance(dto.DeckInstanceId);
+            var deckInstance = await deckRepository.GetDeckInstance(deckInstanceId);
             var flashcard = await flashcardRepository.GetByIdAsync(dto.FlashcardId);
 
             var reviewData = await flashcardReviewService.Review(deckInstance, flashcard!, dto.Quality);
--- Pawlin.Server/Dto/ReviewDto.cs
+++ Pawlin.Server/Dto/ReviewDto.cs
@@ -3,7 +3,6 @@
     public class ReviewDto
     {
         public int FlashcardId { get; set; }
-        public int DeckInstanceId { get; set; }
         public int Quality { get; set; }
     }
 }
