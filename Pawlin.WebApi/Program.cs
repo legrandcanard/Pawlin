7eeb0b227e45341e7b71dbd48fb9469ef14dd5e7
Vladislav Mironov <mironov.v.v@outlook.com>
2025-10-23 16:50:22 +0300

    Stagger enabled for all envs

--- Pawlin.WebApi/Program.cs
+++ Pawlin.WebApi/Program.cs
@@ -49,7 +49,7 @@ app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
